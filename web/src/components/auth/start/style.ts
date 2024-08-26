c7600ffcc2dac40a6fb89ea1556744052da15c60
해준 <127281057+ftery0@users.noreply.github.com>
2024-08-27 04:05:41 +0900

    Configure Azure Developer Pipeline

--- web/src/components/auth/start/style.ts
+++ web/src/components/auth/start/style.ts
@@ -1,5 +1,16 @@
-import styled from "styled-components";
+import styled,{keyframes} from "styled-components";
 import mainRectangle from "../../assets/Group 3.svg";
+const mouthAni = keyframes`
+0%{
+    left: 35px;
+}
+80%{
+    transform: matrix(100deg);
+}
+100%{
+    left: 23px;
+}
+`
 
 export const auth = styled.div`
     position: absolute;
@@ -10,7 +21,8 @@ export const auth = styled.div`
     display: flex;
     flex-direction: column;
     align-items: center;
-
+    opacity: 1;
+    translate: 1s ease-in-out;
 `
 export const main = styled.div`
 display: flex;
@@ -88,4 +100,27 @@ font-weight: 800;
 line-height: normal;
 }
 
+`
+export const onBordingSrceen  =styled.div`
+display: flex;
+align-items: center;
+justify-content: center;
+width: 100vw;
+height: 100vh;
+background-color: #fff;
+`
+export const Logo = styled.div`
+position: relative;
+display: flex;
+width: 166px;
+height: 184px;
+`
+export const mouthImg = styled.img`
+position: absolute;
+width: 24.76px;
+height: 24.76px;
+top: 125px;
+animation-name: ${mouthAni};
+animation-duration: 1s;
+animation-fill-mode: forwards;
 `
