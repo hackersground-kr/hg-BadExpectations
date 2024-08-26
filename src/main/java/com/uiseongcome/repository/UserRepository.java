442c3091af9c591501a5c316e5f7fce602ff96a2
김동찬 <129833370+dongchandev@users.noreply.github.com>
2024-08-26 18:00:26 +0900

    feat: guide

--- src/main/java/com/uiseongcome/repository/UserRepository.java
+++ src/main/java/com/uiseongcome/repository/UserRepository.java
@@ -4,5 +4,4 @@ import com.uiseongcome.domain.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface UserRepository extends JpaRepository<User, String> {
-    User findById();
 }
